79bf0c23260b4b89c1344d8e7a104d52b861bfeb
Pete Bachant <petebachant@gmail.com>
2015-04-01 12:49:00 -0400

    Add functions for computing and plotting CFT torque coefficient

--- processing.py
+++ processing.py
@@ -7,8 +7,9 @@ This module contains functions for loading ands plotting data.
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
 
-plt.style.use("ggplot")
+plt.style.use("arial")
 
 Re_list = [8e4, 1.1e5, 1.3e5, 1.6e5, 1.9e5, 2.1e5, 2.4e5, 2.7e5, 2.9e5, 3.2e5,
            3.4e5]
@@ -18,9 +19,10 @@ def load(Re, foil="0020"):
     fname = "NACA {}_T1_Re{:.3f}_M0.00_N9.0.dat".format(foil, Re/1e6)
     fpath = "data/{}/{}".format(foil, fname)
     aoa, cl, cd = np.loadtxt(fpath, skiprows=14, unpack=True)
-    aoa = np.append([0], aoa[:-1])
-    cl = np.append([0], cl[:-1])
-    cd = np.append(cd[0], cd[:-1])
+    if foil == "0020":
+        aoa = np.append([0], aoa[:-1])
+        cl = np.append([0], cl[:-1])
+        cd = np.append(cd[0], cd[:-1])
     df = pd.DataFrame()
     df["aoa"] = aoa
     df["cl"] = cl
@@ -109,13 +111,78 @@ def plot_ct_all(foil="0020"):
         plot_ct(Re, foil, newfig=False)
     plt.xlim((0,30))
     
+def lookup(aoa_deg, Re, foil="0020"):
+    aoa_deg = np.asarray(aoa_deg)
+    df = load(Re, foil)
+    df["aoa_rad"] = df.aoa/180*np.pi
+    f_cl = interp1d(df.aoa, df.cl)
+    f_cd = interp1d(df.aoa, df.cd)
+    f_ct = interp1d(df.aoa, df.cl*np.sin(df.aoa_rad) - df.cd*np.cos(df.aoa_rad))
+    cl, cd, ct = f_cl(aoa_deg), f_cd(aoa_deg), f_ct(aoa_deg)
+    return {"cl": cl, "cd": cd, "ct": ct}
+    
+def calc_cft_ctorque(Re, tsr=1.9, chord=0.14, R=0.5, foil="0020"):
+    U_infty = 1.0
+    omega = tsr*U_infty/R
+    theta_blade_deg = np.arange(0, 181)
+    theta_blade_rad = theta_blade_deg/180.0*np.pi
+    blade_vel_mag = omega*R
+    blade_vel_x = blade_vel_mag*np.cos(theta_blade_rad)
+    blade_vel_y = blade_vel_mag*np.sin(theta_blade_rad)
+    u = U_infty # No induction
+    rel_vel_mag = np.sqrt((blade_vel_x + u)**2 + blade_vel_y**2)
+    rel_vel_x = u + blade_vel_x
+    rel_vel_y = blade_vel_y
+    relvel_dot_bladevel = (blade_vel_x*rel_vel_x + blade_vel_y*rel_vel_y)
+    alpha_rad = np.arccos(relvel_dot_bladevel/(rel_vel_mag*blade_vel_mag))
+    alpha_deg = alpha_rad*180/np.pi
+    ctorque = lookup(alpha_deg, Re, foil)["ct"]*chord/(2*R)*rel_vel_mag**2/U_infty**2
+    df = pd.DataFrame()
+    df["theta"] = theta_blade_deg
+    df["alpha_deg"] = alpha_deg
+    df["rel_vel_mag"] = rel_vel_mag
+    df["ctorque"] = ctorque
+    return df
+    
+def calc_cft_re_dep(tsr=1.9, chord=0.14, R=0.5, foil="0020"):
+    max_ctorque = []
+    min_ctorque = []
+    for Re in Re_list:
+        df = calc_cft_ctorque(Re, tsr, chord, R, foil)
+        max_ctorque.append(df.ctorque.max())
+        min_ctorque.append(df.ctorque.min())
+    plt.figure()
+    plt.plot(Re_list, max_ctorque, "k")
+    plt.grid()
+    ax = plt.gca()
+    ax.xaxis.major.formatter.set_powerlimits((0,0))
+    plt.xlabel(r"$Re_c$")
+    plt.ylabel(r"Max geometric torque coeff.")
+    plt.tight_layout()
+    
+def plot_cft_ctorque(Re, tsr=1.9, chord=0.14, R=0.5, foil="0020"):
+    df = calc_cft_ctorque(Re, tsr, chord, R, foil)
+    plt.figure(figsize=(10,4))
+    plt.subplot(1, 2, 1)
+    plt.plot(df.theta, df.alpha_deg, "k")
+    plt.xlabel("Azimuthal angle (degrees)")
+    plt.ylabel("Angle of attack (degrees)")
+    plt.subplot(1, 2, 2)
+    plt.plot(df.theta, df.ctorque, "k")
+    plt.xlabel("Azimuthal angle (degrees)")
+    plt.ylabel("Torque coefficient")
+    plt.tight_layout()
+    plt.show()
+    
 if __name__ == "__main__":
-    plot_cl_cd_all("4520")
+#    plot_cl_cd_all("4520")
 #    plot_max_cl()
 #    plot_min_cd()
 #    plot_max_cl_cd()
 #    plot_aoa_max_cl_cd()
 #    plot_ct(1.1e5)
 #    plot_cl_cd(1.1e5)
-    plot_ct_all("0020")
-    plot_ct_all("4520")
+#    plot_ct_all("4520")
+#    plot_ct_all("4520")
+    plot_cft_ctorque(2.1e5, foil="0020")
+    calc_cft_re_dep(foil="0020")
